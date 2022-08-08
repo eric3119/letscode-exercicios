4fb97e5125d0e63a15090be7a8b074f5505557d0
eric3119 <esc2@ic.ufal.br>
2022-08-08 12:37:30 -0300

    spring initializer

---
+++ projeto-final-web/src/main/java/com/bbletscode/rotativo/RotativoApplication.java
@@ -0,0 +1,13 @@
+package com.bbletscode.rotativo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RotativoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RotativoApplication.class, args);
+	}
+
+}
---
+++ projeto-final-web/src/test/java/com/bbletscode/rotativo/RotativoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbletscode.rotativo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RotativoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
